03d49e7357a81c3d00d24ba94455797c9f8b6d38
Udit Narayan Saxena <udit9407@gmail.com>
2025-07-16 15:36:14 +0530

    update in frontend

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -18,7 +18,7 @@ const Contact = () => {
           <p className='font-semibold text-lg text-gray-600'>Careers at PRESCRIPTO</p>
           <p className='text-gray-500'>Learn more about our teams and job openings.</p>
 
-          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore More Jobs</button>
         </div>
       </div>
 
