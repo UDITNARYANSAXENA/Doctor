4e09fcf3b6abf9d388b86958b748b9f159ec6286
Udit Narayan Saxena <udit9407@gmail.com>
2025-07-16 15:34:58 +0530

    update in controller

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -28,14 +28,12 @@ const App = () => {
       <div className='flex items-start'>
         <Sidebar />
         <Routes>
-          {/* Admin Routes */}
           <Route path='/' element={<></>} />
           <Route path='/admin-dashboard' element={<Dashboard />} />
           <Route path='/all-appointments' element={<AllAppointments />} />
           <Route path='/add-doctor' element={<AddDoctor />} />
           <Route path='/doctor-list' element={<DoctorsList />} />
           
-          {/* Doctors Route */}
           <Route path= '/doctor-appointments' element= {<DoctorAppointments/>}/>
           <Route path='/doctor-dashboard' element={<DoctorDashboard/>} />
           <Route path='/doctor-profile' element={<DoctorProfile/>} />
